31d73b5d51ab04aaa25853c93e800e48af0fb0ef
Jandir A. Cutabiala <jandir.dev@gmail.com>
2022-06-25 01:07:03 -0300

    fix: remove props

--- app/index.js
+++ app/index.js
@@ -4,13 +4,12 @@ const app = express()
 const populateDB = require('./utils/populateDB')
 const port = 3000
 
-populateDB()
-
 app.set('view engine', 'pug')
 app.set('views', './views')
 
 app.use(require('./routes/people'))
 
 app.listen(port, () => {
+    populateDB()
     console.log(`Listening on port ${port}`)
 })
